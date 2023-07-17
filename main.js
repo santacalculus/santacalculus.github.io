4894ad070c91d43efe2d554e5c31d248b3666c93
santacalculus <lchintar@andrew.cmu.edu>
2023-07-17 00:33:31 -0400

    cleaned up the JS file and removed unnecessary commented out code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 import {animate, scroll, inView} from 'https://cdn.skypack.dev/motion';
 
+// animations for the about section and profile photo
 window.onload = () => {
   // prevent FOUC (flash of unstyled content)
   console.log("what")
@@ -18,7 +19,7 @@ window.onload = () => {
 };
 
 
-
+// this is for the hovering animations for descriptions
 const selflink = document.getElementById("id_selflink");
 const selfdesc = document.getElementById("id_selfdesc");
 const proofdesc = document.getElementById("id_proofdesc");
@@ -77,8 +78,6 @@ document.querySelectorAll("section").forEach((item, index) => {
   // more fun code to check when the section comes into view and change the image accordingly
   scroll(() => {
     inView(item, () => {
-      
-        // console.log("ok");
         if (index == 0) { 
           const image = document.getElementById("id_profile");
           if (image) {
@@ -107,42 +106,11 @@ document.querySelectorAll("section").forEach((item, index) => {
 
   // more cute and simple code to change the color of the text in section when it comes into view <3
   if (index !== 0 && text) {
-    // console.log(text);
     scroll(animate(text, { color: ["rgb(33,23,31)", "rgb(51, 51, 0)", "rgb(51, 51, 0)", "rgb(33,23,31)"] }), {
       target: text,
       offset: ["center end", "end end", "start start", "center start"],
     });
   }
-
-  
-
-  // // changes the font color of the section when more than 80% of it is in the view
-  // function checkView(isInView) {
-  //   if (isInView) {
-  //     console.log("Here");
-  //     const text = item.querySelectorAll("p");
-      
-  //     if (index !== 0) {
-  //       text.forEach((text) => {
-  //       text.style.color = "rgb(51, 51, 0)";
-  //       });
-  //     }
-  //   } else {
-  //     console.log("huh");
-  //     const text = item.querySelectorAll("p");
-  //     text.forEach((text) => {
-  //       text.style.color = ""; // Reset to the original color or remove inline style
-  //     });
-  //   }
-  // }
-
-  // console.log(item);
-  
-  // inView(item, checkView, { amount: 0.9 })
   
 });
 
-
-// animate("#id_profile", {y: 10}, {delay: stagger(0.1),
-//   duration: 0.5,
-//   easing: "ease-in"})
